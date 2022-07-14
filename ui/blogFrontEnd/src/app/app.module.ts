7152afffc025dd6ca63f549207e9789bb9072024
Jacob Guiro <jacob.guiro@zealitconsultants.com>
2022-07-14 17:32:31 -0500

    Fixed Bug where Modules were not imported into App module

--- ui/blogFrontEnd/src/app/app.module.ts
+++ ui/blogFrontEnd/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AuthModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
