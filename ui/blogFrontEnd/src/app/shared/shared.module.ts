5351d2caea40f1b8610168925f43f82737afef93
david44868 <davidhariantowu@gmail.com>
2022-07-15 21:39:53 -0400

    Login page html

--- ui/blogFrontEnd/src/app/shared/shared.module.ts
+++ ui/blogFrontEnd/src/app/shared/shared.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, MatButtonModule
+    CommonModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   exports: [
+    MatFormFieldModule, 
+    MatInputModule, 
+    FormsModule,
+    ReactiveFormsModule,
     MatButtonModule
   ]
 })
